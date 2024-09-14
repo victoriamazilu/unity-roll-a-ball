5534eb4e0b7575cb511b5d4ea4900d6765587215
victoriamazilu <victoria.mazilu@icloud.com>
2024-09-14 15:45:23 -0400

    Add prefab pickup logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -28,4 +28,12 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
         rb.AddForce(movement * speed); 
     }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("PickUp")) 
+        {
+            other.gameObject.SetActive(false);
+        }
+   }
 }
