10ce9e5eea78adcc33d06436ea988bec614b8e9b
victoriamazilu <victoria.mazilu@icloud.com>
2024-09-14 16:15:08 -0400

    Adding display texts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb; 
+    private int count;
+    public GameObject winTextObject;
+
     private float movementX;
     private float movementY;
 
     public float speed = 0;
+    public TextMeshProUGUI countText;
 
     void Start()
     {
         rb = GetComponent <Rigidbody>(); 
+        count = 0; 
+
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -21,6 +30,16 @@ public class PlayerController : MonoBehaviour
         Vector2 movementVector = movementValue.Get<Vector2>(); 
         movementX = movementVector.x; 
         movementY = movementVector.y; 
+    }
+
+    void SetCountText() 
+    {
+        countText.text =  "Count: " + count.ToString();
+
+        if (count >= 10)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 
     private void FixedUpdate() 
@@ -34,6 +53,9 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp")) 
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
         }
    }
 }
